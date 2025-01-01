Added screenshot processing to simplify training and fixed rotation resetting for multiple environments

Main.py
@@ -1,19 +1,24 @@
 # hyperparameters
 import time
 
+import numpy as np
 import tqdm
 import rich
 import gymnasium as gym
 from gymnasium.vector import AsyncVectorEnv
 from gymnasium.wrappers import TimeLimit
-from stable_baselines3 import DDPG, A2C, SAC, DQN, PPO
+from stable_baselines3 import DDPG, A2C, SAC, DQN, PPO, TD3
 from stable_baselines3.common.callbacks import CheckpointCallback, BaseCallback
+from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.vec_env import VecFrameStack, StackedObservations, VecMonitor, DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.noise import NormalActionNoise, VectorizedActionNoise
+from stable_baselines3.common.vec_env import VecFrameStack, StackedObservations, VecMonitor, DummyVecEnv, SubprocVecEnv, \
+    VecVideoRecorder
 from stable_baselines3.common.callbacks import ProgressBarCallback
 
-from stable_baselines3.sac import CnnPolicy
+# from stable_baselines3.sac import CnnPolicy
 # from stable_baselines3.ppo import CnnPolicy
+from stable_baselines3.td3 import CnnPolicy
 
 
 from ZumaInterface.envs.ZumaEnv import ZumaEnv
@@ -41,7 +46,8 @@ def make_env(env_name, env_index):
     def _make():
         env = gym.make(env_name,
                        env_index=env_index,
-                       max_episode_steps=500)
+                       max_episode_steps=250)
+        # check_env(env)
         # env = ZumaEnv(env_index=env_index)
         # env = TimeLimit(env, 500)
         return env
@@ -64,46 +70,86 @@ if __name__ == "__main__":
 
     env_monitor = VecMonitor(env_vec)
     env_stacked = VecFrameStack(env_monitor, 2)
+    # env_stacked = VecVideoRecorder(env_monitor, "logs/",
+    #                        record_video_trigger=lambda x: x == 0, video_length=20)
     # env = ResizeObservation(env, (100, 100))
     print(env_stacked.observation_space)
     # check_env(env)
     checkpoint_callback = CheckpointCallback(save_freq=10_000,
                                              save_path='./model_checkpoints/')
 
-    model = SAC(CnnPolicy,
-                env_stacked,
-                learning_rate=3e-4,
-                learning_starts=1,
-                buffer_size=500_000,
-                batch_size=10_000,
-                gamma=0.99,
-                tau=0.005,
-                train_freq=1,
-                gradient_steps=1,
-                ent_coef="auto",
-                verbose=1,
-                tensorboard_log="./tensorboard_logs/",
-                device="cuda"
-                )
+    n_actions = env_monitor.action_space.shape[-1]
+    base_noise = NormalActionNoise(mean=np.zeros(1), sigma=0.1 * np.ones(n_actions))
+    # vec_noise = VectorizedActionNoise(base_noise, 10)
+    # model = SAC.load("./models/model")
+    # model.action_noise = vec_noise
+    # model.ent_coef = -1.0
+    # model.set_env(env_stacked)
+
+    # model = SAC(CnnPolicy,
+    #             env_stacked,
+    #             learning_rate=3e-4,
+    #             learning_starts=1,
+    #             buffer_size=30_000,
+    #             batch_size=256,
+    #             gamma=0.98,
+    #             tau=0.005,
+    #             train_freq=1,
+    #             gradient_steps=1,
+    #             ent_coef=1.0,
+    #             # target_entropy=1.0,
+    #             verbose=1,
+    #             tensorboard_log="./tensorboard_logs/",
+    #             device="cuda",
+    #             action_noise=vec_noise,
+    #             policy_kwargs={"use_sde": True},
+    #             )
 
     # model = PPO(CnnPolicy,
     #             env_stacked,
     #             n_steps=128,
     #             n_epochs=4,
-    #             batch_size=128,
+    #             batch_size=256,
     #             learning_rate=0.00025,
     #             clip_range=0.1,
     #             vf_coef=0.5,
     #             ent_coef=0.01,
     #             verbose=1,
-    #             # tensorboard_log="./tensorboard_logs/",
-    #             # device="cpu",
-    #
+    #             tensorboard_log="./tensorboard_logs/",
+    #             device="cuda",
     #             )
 
-    model.learn(total_timesteps=3_000_000,
+    # model = SAC(CnnPolicy,
+    #             env_stacked,
+    #             learning_rate=3e-4,
+    #             learning_starts=1,
+    #             buffer_size=50_000,
+    #             batch_size=256,
+    #             gamma=0.5,
+    #             tau=0.005,
+    #             train_freq=1,
+    #             gradient_steps=1,
+    #             verbose=1,
+    #             tensorboard_log="./tensorboard_logs/",
+    #             device="cuda",
+    #             # action_noise=vec_noise,
+    #             # policy_kwargs={"use_sde": True},
+    #             )
+    model = TD3(CnnPolicy,
+                env_stacked,
+                buffer_size=100_000,
+                gamma=0.95,
+                verbose=1,
+                tensorboard_log="./tensorboard_logs/",
+                device="cuda",
+                action_noise=base_noise,
+                # optimize_memory_usage=True
+                )
+
+    model.learn(total_timesteps=4_000_000,
                 log_interval=1,
                 callback=[checkpoint_callback],
+                # reset_num_timesteps=False,
                 )
 
     model.save("./models/model")
@@ -117,7 +163,7 @@ if __name__ == "__main__":
 
 
     # NOTE: Env test
-    # env = ZumaEnv()
+    # env = ZumaEnv(0)
     # obs, info = env.reset()
     # n_steps = 10
     # for i in range(n_steps):

StateReader.py
@@ -93,9 +93,30 @@ class StateReader:
         lives_offsets = [0x60, 0x88, 0x8, 0x98, 0x48, 0xC0]
         self.lives_addr = StateReader._read_offsets(self.process, lives_base_addr, lives_offsets)
 
-        rotation_base_addr = base_address + 0x00133DA0
-        rotation_offsets = [0x4, 0x4, 0x14, 0x34, 0x1FC, 0x4]
-        self.rotation_addr = StateReader._read_offsets(self.process, rotation_base_addr, rotation_offsets)
+        try:
+            # Possible address 1
+            rotation_base_addr = base_address + 0x00133DA0
+            rotation_offsets = [0x4, 0x4, 0x14, 0x34, 0x1FC, 0x4]
+            self.rotation_addr = StateReader._read_offsets(self.process, rotation_base_addr, rotation_offsets)
+            rotation = float(r_float(self.process, self.rotation_addr))
+            if rotation == 0:
+                raise Exception
+            self.reset_rotation()
+        except:
+            try:
+                # Possible address 2
+                rotation_base_addr = base_address + 0x00148078
+                rotation_offsets = [0xD0, 0x94, 0xC0, 0x38, 0xC0, 0x210, 0x4]
+                self.rotation_addr = StateReader._read_offsets(self.process, rotation_base_addr, rotation_offsets)
+                rotation = float(r_float(self.process, self.rotation_addr))
+                if rotation == 0:
+                    raise Exception
+            except:
+                # Possible address 2
+                rotation_base_addr = base_address + 0x00132F90
+                rotation_offsets = [0xA0, 0x544, 0x8, 0x84, 0x4, 0x268, 0x4]
+                self.rotation_addr = StateReader._read_offsets(self.process, rotation_base_addr, rotation_offsets)
+                rotation = float(r_float(self.process, self.rotation_addr))
 
         focus_loss_base_addr = base_address + 0x0017FF54
         focus_loss_offsets = [0x58, 0xC8, 0x8, 0x5C, 0x48, 0xCC]
@@ -113,8 +134,7 @@ class StateReader:
         self.score = int(r_int(self.process, self.score_addr))
         self.progress = int(r_int(self.process, self.progress_addr))
         self.lives = int(r_int(self.process, self.lives_addr))
-        # TODO
-        # self.rotation = float(r_float(self.process, self.rotation_addr))
+        self.rotation = float(r_float(self.process, self.rotation_addr))
 
     def write_game_values(self):
         w_int(self.process, self.score_addr, self.score)
@@ -210,17 +230,18 @@ class StateReader:
         img = Image.frombuffer(
             'RGB',
             (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
-            bmpstr, 'raw', 'BGRX', 0, 1
+            bmpstr,
+            'raw', 'BGRX', 0, 1
         )
         img = img.crop((15, 30, bmpinfo['bmWidth']-15, bmpinfo['bmHeight']-15))
-
+        # img = img.convert("BGR")
         # img = StateReader.transform_image_to_2bit_rgb(img)
 
         # transformed_image = StateReader.transform_image_to_2bit_rgb(img)
 
         win32gui.DeleteObject(bmp.GetHandle())
 
-        img = img.resize((40, 40))
+        # img = transformed_image.resize((160, 160))
 
         return img
 
@@ -238,9 +259,9 @@ class StateReader:
         # Combine the channels back into an image
         transformed_array = np.stack((r_2bit, g_2bit, b_2bit), axis=-1)
         transformed_image = Image.fromarray(transformed_array.astype('uint8'), mode="RGB")
-        resized_image = transformed_image.resize((84, 84))
+        # resized_image = transformed_image.resize((84, 84))
 
-        return resized_image
+        return transformed_image
 
     # def __del__(self):
         # win32gui.ReleaseDC(self.hwnd, self.hwindc)

envs/ZumaEnv.py
@@ -1,5 +1,7 @@
 import threading
 from typing import Optional
+
+import cv2
 import numpy as np
 import gymnasium as gym
 import time
@@ -7,6 +9,7 @@ import time
 import pyautogui
 import win32gui
 
+from ZumaInterface.ImageProcessing import ImageProcessing
 from ZumaInterface.StateReader import StateReader
 
 
@@ -19,24 +22,25 @@ class ZumaEnv(gym.Env):
 
         self.state_reader = StateReader(env_index=env_index)
 
-        self.step_delay_s = 0.04
+        self.step_delay_s = 0.3
         self.max_delay_ms = 1000
         self.max_delay_rollback = 1500
         self.playable = True
-        self.reset_delay = 2.5
+        self.reset_delay = 3
         self.reset_delay_start = 0
 
         # Observations are dictionaries with the agent's and the target's location.
         # Each location is encoded as an element of {0, ..., `size`-1}^2
-        self.observation_space = gym.spaces.Box(0, 255, shape=(40, 40, 3), dtype=np.uint8)
+        self.observation_space = gym.spaces.Box(0, 255, shape=(80, 112, 3), dtype=np.uint8)
 
-        self.action_space = gym.spaces.Box(low=np.array([-1.0]), high=np.array([1.0]), dtype=np.float64)
+        self.action_space = gym.spaces.Box(low=-1.0, high=1.0, dtype=np.float32)
 
     def _get_obs(self):
         # self.state_reader.stack_frames(False)
         img = self.state_reader.screenshot_process()
-        img_arr = np.array(img)
-        return img_arr
+        img = ImageProcessing.prepare_image(img)
+        # img_arr = np.array(img)
+        return img
 
     def _get_info(self):
         self.state_reader.read_game_values()
@@ -79,7 +83,7 @@ class ZumaEnv(gym.Env):
 
         old_score = self.state_reader.score
 
-        reward = 0
+        reward = -0.01
         terminated = False
         if time.time() - self.reset_delay_start > self.reset_delay:
             self.playable = True
@@ -87,8 +91,7 @@ class ZumaEnv(gym.Env):
         if self.playable:
             self.state_reader.shoot_ball(angle)
             time.sleep(self.step_delay_s/2)
-            # TODO
-            # self.state_reader.reset_rotation()
+            self.state_reader.reset_rotation()
             time.sleep(self.step_delay_s/2)
 
             self.state_reader.read_game_values()
@@ -100,17 +103,17 @@ class ZumaEnv(gym.Env):
                 self.state_reader.write_game_values()
                 self.playable = False
                 self.reset_delay_start = time.time()
+                reward = -1
+            else:
+                new_score = self.state_reader.score
+                score_change = new_score - old_score
+                # if score_change > 0:
+                #     reward = 1
+                if score_change > 100:
+                    reward = 1
+                elif score_change > 0:
+                    reward = 0.5
 
-            new_score = self.state_reader.score
-            score_change = new_score - old_score
-            if score_change > 100:
-                reward = 1
-            elif score_change > 0:
-                reward = 0.5
-            # elif terminated:
-            #     reward = -1
-            # else:
-            #     reward = -0.01
         else:
             self.state_reader.shoot_ball(180)
             time.sleep(self.step_delay_s)
@@ -124,6 +127,8 @@ class ZumaEnv(gym.Env):
     def render(self, mode="rgb_array"):
         if mode == "rgb_array":
             img = self.state_reader.screenshot_process()
+            img = ImageProcessing.prepare_image(img)
+
             return img
 
     # NOTE: old